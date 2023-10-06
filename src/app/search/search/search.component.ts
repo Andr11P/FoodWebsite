1cce272d7eb9a30e7a29616e92acb7622c54a4b2
Andr11P <128001519+Andr11P@users.noreply.github.com>
2023-10-05 19:55:25 -0700

    Update search.component.ts

--- src/app/search/search/search.component.ts
+++ src/app/search/search/search.component.ts
@@ -11,6 +11,7 @@ export class SearchComponent implements OnInit {
   searchTerm: String = "";
   constructor(private route: ActivatedRoute, private router:Router) { }
 
+  // checking if item on the site
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       if (params.searchTerm)
@@ -18,6 +19,7 @@ export class SearchComponent implements OnInit {
     })
   }
 
+  // searching method
   search():void{
     if(this.searchTerm)
     this.router.navigateByUrl('/search/' + this.searchTerm);
